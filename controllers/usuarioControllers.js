78bb44771867e778e6ee853adfb09b4f8b342642
fariascode <fariasalf70@gmail.com>
2024-11-09 17:31:50 -0600

    Base de Datos
    Actualizacion de la BD

--- controllers/usuarioControllers.js
+++ controllers/usuarioControllers.js
@@ -1,3 +1,4 @@
+import {check, validationResult} from 'express-validator'
 import User from "../models/Usuario.js"
 
 const formularioLogin = (req,res) => {
@@ -13,9 +14,31 @@ const formularioRegistro = (req,res) => {
 }
 
 const register = async (req,res) => {
+    // Validacion
+    await check('nombre').notEmpty().withMessage('El nombre no puede ir vacio').run(req)
+    await check('email').isEmail().withMessage('Eso no parece un email').run(req)
+    await check('password').isLength({min: 6}).withMessage('El password debe ser de almenos 6 caracteres').run(req)
+    await check('repite_password').equals('password').withMessage('Los passwords no son iguales').run(req)
+
+    let resultado = validationResult(req)
+
+   
+    //Verificar que el resutado este vacio
+    if(!resultado.isEmpty()){
+        //errores
+        return res.render('auth/registro', {  
+        pagina: 'Crear Cuenta',
+        errores: resultado.array(), 
+        usuario: {
+            nombre: req.body.nombre,
+            email: req.body.email
+        }
+        })
+    }
+
+    res.json()
 
     const user = await Usuario.create(req.body)
-
     res.json(user)
 }
 
